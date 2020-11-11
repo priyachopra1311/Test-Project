a8c564de220fc4517b92cdd83fa4975b0faa97ad
Priya Chopra <priyachopra1311@gmail.com>
2020-11-11 12:36:36 +0530

    icons added

--- chatbot_test.js
+++ chatbot_test.js
@@ -1,7 +1,17 @@
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
-}
 
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
+//function openForm() {
+//  var popup = document.getElementById("myForm");
+//  popup.classList.toggle("show");
+//}
+
+function myFunction() {
+  var popup = document.getElementById("myForm");
+  if (popup.style.display == "none") {
+      popup.style.display = "block";
+      $("#chatid").attr("src", "close_icon.png");  
+  }
+  else {
+      popup.style.display = "none";
+      $("#chatid").attr("src", "chat_icon.png");
+  }
 }
